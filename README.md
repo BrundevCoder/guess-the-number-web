5ce910b6bceb23392a304920e6ba3f4bd024d80e
BrundevCoder <brunodevdrums@gmail.com>
2026-04-29 18:48:54 +0100

    fixed bugs, implemented new features

---
+++ README.md
@@ -0,0 +1,14 @@
+### Guess The number in web!
+
+## How to play 👾
+First you click on the start Button, then the website prompts you to you try to guess the secret number randomly choosen, you can try how many times you want. As many people requested, I added an attempt counter, and also the functionality to press the Enter key (only in computers) instead of only being able to try by clicking the 'Try!' button.
+
+## Design
+I tried to improve the design, such as by adding a new font and changing some other elements, like the new green background 🟢, and give it a pixelated vibe, and also changing the color of other things.
+
+## Why did I create this??
+Well, i created because i was trying to manipulate the DOM of the webpage, i enjoy trying someting new. I'm liking to go ahead with this project
+
+# key:
+
+`Enter` just to send your guess.
--- script.js
+++ script.js
@@ -8,15 +8,81 @@ const message = document.getElementById("message");
 const userInput = document.getElementById("userInput");
 const inputButton = document.getElementById("sendGuessBtn");
 const restartButton = document.getElementById("refresh-btn");
+const firstTriesDisplay = document.getElementById("triesDisplay");
+const finalEndTries = document.getElementById("endTries");
 
+// new
+
+const modeScreen = document.getElementById("modeScreen");
+const easyBtn = document.getElementById("easyBtn");
+const mediumBtn = document.getElementById("mediumBtn");
+const hardBtn = document.getElementById("hardBtn");
+
+const detailsButton = document.getElementById("seeDetails");
+const details = document.getElementById("detailsDrop")
+
+const maxNumberDisplay = document.getElementById("maxNumber");
+const modeDisplay = document.getElementById("modeDisplay");
+
+const lostScreen = document.getElementById("lostScreen");
+const livesDisplay = document.getElementById("livesDisplay");
+const restartBtnTow = document.getElementById("refresh-btn2");
+
+const secretButton = document.getElementById("secretButton");
+const secretDisplay = document.getElementById("secretDisplay");
+
+let clicks = 0;
+let showing = false;
 let guess;
 let win = false;
 let secretNumber;
+let mode = "";
+let tries = 0;
+let lives = 0;
 
 // functions
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 100);
+function generateRandomNumber(mode) {
+  if (mode === "easy") {
+    return Math.floor(Math.random() * 50);
+  }
+  else if (mode === "medium") {
+    return Math.floor(Math.random() * 100);
+  }
+  else if (mode === "hard") {
+    return Math.floor(Math.random() * 500);
+  }
+  else {
+    return Math.floor(Math.random() * 100);
+  }
+}
+
+function handleMode(modeType) {
+  // set mode variable to the given argument
+  const modeMaxInfo = {
+    easy: 50,
+    medium: 100,
+    hard: 500,
+  };
+
+  mode = modeType;
+
+  let max = 0;
+
+  mode === "easy" ? max = modeMaxInfo["easy"] : max = max;
+  mode === "medium" ? max = modeMaxInfo["medium"] : max = max;
+  mode === "hard" ? max = modeMaxInfo["hard"] : max = max;
+
+  lives = ( modeMaxInfo[mode] / 10 ) * 2
+
+  modeDisplay.innerText = mode.charAt(0).toUpperCase() + mode.slice(1);
+
+  modeDisplay.classList.add(mode);
+
+  modeScreen.classList.remove("active");
+  startScreen.classList.add("active");
+
+  maxNumberDisplay.innerText = max;
 }
 
 function getUserInput() {
@@ -25,17 +91,23 @@ function getUserInput() {
 }
 
 function startGame() {
-  secretNumber = generateRandomNumber();
-  console.log(secretNumber);
+  secretNumber = generateRandomNumber(mode);
 
   // control the screens
   startScreen.classList.remove("active");
   mainScreen.classList.add("active");
+
+  userInput.focus();
 }
 
 function handleWin() {
   WinScreen.classList.add("active");
   mainScreen.classList.remove("active");
+}
+
+function handleLose() {
+  lostScreen.classList.add("active")
+  mainScreen.classList.remove("active")
 }
 
 function handleGame() {
@@ -44,20 +116,88 @@ function handleGame() {
 
   if (guess < secretNumber) {
     message.innerText = `The number is bigger than ${guess}`;
+    tries++;
+    lives--;
+    firstTriesDisplay.innerText = `Tries: ${tries}`;
+    finalEndTries.innerHTML = `Tries: ${tries}`;
   }
   else if (guess > secretNumber) {
     message.innerText = `The number is lower than ${guess}`;
+    tries++;
+    lives--;
+    firstTriesDisplay.innerText = `Tries: ${tries}`;
+    finalEndTries.innerHTML = `Tries: ${tries}`;
   }
   else {
+    tries++;
+    firstTriesDisplay.innerText = `Tries: ${tries}`;
+    finalEndTries.innerHTML = `Tries: ${tries}`;
     // win
     win = true;
     handleWin();
   }
+
+  if (lives === 0) {
+    handleLose()
+  }
+
+  livesDisplay.innerText = lives;
 }
 
 // connect buttons
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", () => {
+  startGame();
+  livesDisplay.innerText = lives;
+});
 inputButton.addEventListener("click", handleGame);
+userInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    handleGame();
+  }
+});
 restartButton.addEventListener("click", () => {
   location.reload();
+})
+restartBtnTow.addEventListener("click", () => {
+  location.reload();
+})
+
+// mode buttons
+easyBtn.addEventListener("click", () => {
+  handleMode("easy");
+})
+mediumBtn.addEventListener("click", () => {
+  handleMode("medium");
+})
+hardBtn.addEventListener("click", () => {
+  handleMode("hard");
+})
+
+detailsButton.addEventListener("click", () => {
+  details.classList.toggle("visible");
+})
+secretButton.addEventListener("click", () => {
+  clicks++;
+  console.log(clicks)
+
+  if (clicks >= 50) {
+    if (showing) {
+      return;
+    }
+
+    lives += 15;
+    livesDisplay.innerText = lives;
+    
+    secretDisplay.style.visibility = "visible";
+    showing = true;
+
+    setTimeout(() => {
+      secretDisplay.style.visibility = "hidden";
+      showing = false;
+      clicks = 0;
+    }, 1000);
+
+    // reset clicks
+    clicks = 0;
+  }
 })
